018d7562c497c446c2af778a52a72de2092ba1e2
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-05-18 08:34:43 +0300

    Added a support for registering the window class.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <windows.h>
 
+auto CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LPARAM {
+  return DefWindowProc(hwnd, msg, lParam, wParam);
+}
+
 auto WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmd, int show) -> int {
   UNREFERENCED_PARAMETER(instance);
   UNREFERENCED_PARAMETER(prevInstance);
   UNREFERENCED_PARAMETER(cmd);
   UNREFERENCED_PARAMETER(show);
-  
+
+  // register a window class for the application.
+  WNDCLASS windowClass = {};
+  windowClass.lpszClassName = TEXT("d3d11-sprite-rendering");
+  windowClass.lpfnWndProc = WndProc;
+  if (RegisterClass(&windowClass) == 0) {
+    OutputDebugStringA("Failed to register window class!");
+    return -1;
+  }
+
   // TODO
 
   return 0;
