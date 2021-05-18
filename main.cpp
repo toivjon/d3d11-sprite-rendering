f785a9bfd6fbf0a6bbc5036ed198f4b0b0724725
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-05-18 08:22:27 +0300

    Added the initial project files.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+auto WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmd, int show) -> int {
+  UNREFERENCED_PARAMETER(instance);
+  UNREFERENCED_PARAMETER(prevInstance);
+  UNREFERENCED_PARAMETER(cmd);
+  UNREFERENCED_PARAMETER(show);
+  
+  // TODO
+
+  return 0;
+}
