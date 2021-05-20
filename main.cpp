5a3fc2920306873e5875671747bd81b952348949
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2021-05-20 19:33:51 +0300

    Added a simple way to initialize the main window for the application.

--- main.cpp
+++ main.cpp
@@ -1,45 +1,51 @@
 #include <windows.h>
 
+#include <exception>
+
 auto CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LPARAM {
+  switch (msg) {
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      return 0;
+  }
   return DefWindowProc(hwnd, msg, lParam, wParam);
 }
 
-auto WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmd, int show) -> int {
-  UNREFERENCED_PARAMETER(instance);
-  UNREFERENCED_PARAMETER(prevInstance);
-  UNREFERENCED_PARAMETER(cmd);
-  UNREFERENCED_PARAMETER(show);
-
-  // register a window class for the application.
+auto createWindow(HINSTANCE instance, int show) -> HWND {
+  // Register a window class for the application.
   WNDCLASS windowClass = {};
   windowClass.lpszClassName = TEXT("d3d11-sprite-rendering");
   windowClass.lpfnWndProc = WndProc;
+  windowClass.hInstance = instance;
   if (RegisterClass(&windowClass) == 0) {
-    OutputDebugStringA("Failed to register window class!");
-    return -1;
+    throw std::exception("Failed to register window class!");
   }
 
-  // build and show the application window.
-  auto window = CreateWindow(
-    windowClass.lpszClassName,
-    TEXT("D3D11 - Sprite Rendering"),
-    WS_OVERLAPPEDWINDOW,
-    CW_USEDEFAULT,
-    CW_USEDEFAULT,
-    CW_USEDEFAULT,
-    CW_USEDEFAULT,
-    nullptr,
-    nullptr,
-    instance,
-    nullptr
-  );
+  // Build and show a new window that uses the registered window class.
+  auto window = CreateWindow(windowClass.lpszClassName, TEXT("D3D11 - Sprite Rendering"),
+                             WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                             CW_USEDEFAULT, nullptr, nullptr, instance, nullptr);
   if (window == nullptr) {
-    OutputDebugStringA("Failed to create a new window!");
-    return -1;
+    throw std::exception("Failed to create window!");
   }
   ShowWindow(window, show);
+  UpdateWindow(window);
+  return window;
+}
 
-  // TODO
+auto WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmd, int show) -> int {
+  UNREFERENCED_PARAMETER(prevInstance);
+  UNREFERENCED_PARAMETER(cmd);
+  try {
+    createWindow(instance, show);
+
+    MSG message = {};
+    while (GetMessage(&message, nullptr, 0, 0)) {
+      TranslateMessage(&message);
+      DispatchMessage(&message);
+    }
+  } catch (...) {
+  }
 
   return 0;
 }
